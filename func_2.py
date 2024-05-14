3cc25782c60be4ff517d44be9b380293869aabdb
Ta Hong Phuc <taphuc1@gmail.com>
2024-05-14 22:44:00 +0700

    “update”

--- func_2.py
+++ func_2.py
@@ -9,3 +9,32 @@ def create_borrow_card(card_id, borrower_id, borrow_date, expected_return_date):
     
     return "Borrow card created successfully"
 
+def add_book_to_borrow_card(card_id, book_id):
+    # Check if book is available
+    # SQL to check current copies
+    check_sql = "SELECT current_number_of_copies FROM library.Book WHERE book_id = %s"
+    # result = execute_sql(check_sql, (book_id,))
+    
+    # if result[0]['current_number_of_copies'] > 0:
+    #     # SQL to insert borrow card item
+    #     insert_sql = """
+    #     INSERT INTO BorrowCardItem (card_id, book_id)
+    #     VALUES (%s, %s)
+    #     """
+    #     # Execute SQL
+    #     # execute_sql(insert_sql, (card_id, book_id))
+    #     
+    #     # Update book copies
+    #     update_sql = """
+    #     UPDATE library.Book
+    #     SET current_number_of_copies = current_number_of_copies - 1
+    #     WHERE book_id = %s
+    #     """
+    #     # execute_sql(update_sql, (book_id,))
+    #     
+    #     return "Book added to borrow card successfully"
+    # else:
+    #     return "Book not available"
+    
+    # Placeholder return for this example
+    return "Function executed"
